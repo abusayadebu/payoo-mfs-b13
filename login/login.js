63c9f7313e84709a21dc7c73f4ba4415e70d2ee6
abusayadebu <abusayad743@gmail.com>
2026-02-19 01:28:32 +0600

    login data validation done and redirected user to the home page

---
+++ login/login.js
@@ -0,0 +1,17 @@
+// click the button
+const loginBtn = document.getElementById("login-btn").addEventListener("click", function(){
+    // catch the number filed value
+    let givenNumber = document.getElementById("number-input").value;
+    
+    let givenPin = document.getElementById("pin-input").value;
+    console.log(givenNumber, givenPin);
+
+    if(givenNumber === "1234567890" && givenPin == "1234"){
+        console.log("welcome you are logged in")
+        window.location.assign("./home.html")
+        }
+        else{
+            alert("sorry, wrong credentials")
+        }
+    
+})
