51d3fe36b8fa5b698e7d4ea403a92da70a01f258
abusayadebu <abusayad743@gmail.com>
2026-03-09 19:26:16 +0600

    toggle button functionality added between addmoney btn and cashout btn

---
+++ features/functionalities.js
@@ -0,0 +1,30 @@
+// catch the cashOut section
+const cashOutSection = document.getElementById("cash-out-section")
+const addMoneySection = document.getElementById("add-money-section")
+
+const cashOutMainBtn = document.getElementById("cashout-main-btn")
+const addMoneyMainBtn = document.getElementById("addmoney-main-btn")
+const logOutBtn = document.getElementById("log-out-btn")
+
+
+// click and show cashout section
+cashOutMainBtn.addEventListener("click", function(){
+    addMoneyMainBtn.classList.remove("bg-blue-600")
+    cashOutMainBtn.classList.add("bg-blue-600")
+    addMoneySection.classList.add("hidden")
+    cashOutSection.classList.remove("hidden")
+})
+
+// // click and show add money section
+addMoneyMainBtn.addEventListener("click", function(){
+    cashOutMainBtn.classList.remove("bg-blue-600")
+    addMoneyMainBtn.classList.add("bg-blue-600")
+    addMoneySection.classList.remove("hidden")
+    cashOutSection.classList.add("hidden")
+})
+
+
+// click log out and send to the home page
+logOutBtn.addEventListener("click", function(){
+    window.location.href = '/index.html';
+})
